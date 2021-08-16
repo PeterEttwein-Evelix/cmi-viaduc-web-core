ebdfe5dac163c3b6dbe5450f04c4dbca7bca3286
PeterEttwein-Evelix <PEttwein@evelix.ch>
2021-08-16 11:25:50 +0200

    v.1.3.4.5

--- cmi-viaduc-web-core/src/lib/core/components/ui/dialog-service/modal/modal.service.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'cmi-viaduc-modal-service',
-	template: '<ng-content></ng-content>'
-})
-
-export class ModalServiceComponent {
-}
